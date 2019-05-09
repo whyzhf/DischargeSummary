de86a7d76fc8af7774688dc96c8ff73e5f2cb5e8
whyzhf <985843242@qq.com>
2019-05-09 11:41:24 +0800

    ４.２９ 分词

--- src/main/java/com/szgwnet/discharge/DischargeApplication.java
+++ src/main/java/com/szgwnet/discharge/DischargeApplication.java
@@ -46,6 +46,7 @@ public class DischargeApplication extends SpringBootServletInitializer {
         // 方法的调用线程中运行被拒绝的任务（setWaitForTasksToCompleteOnShutdown）；如果执行程序已关闭，则会丢弃该任务
         // setWaitForTasksToCompleteOnShutdown（true）该方法就是这里的关键，用来设置线程池关闭的时候等待所有任务都完成再继续销毁其他的Bean，这样这些异步任务的销毁就会先于Redis线程池的销毁。
         // 同时，这里还设置了setAwaitTerminationSeconds(60)，该方法用来设置线程池中任务的等待时间，如果超过这个时候还没有销毁就强制销毁，以确保应用最后能够被关闭，而不是阻塞住。
+
         @Bean("taskExecutor")
         public Executor taskExecutor() {
             ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
